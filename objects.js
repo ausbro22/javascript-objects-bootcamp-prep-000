78eeb7e5300663eb301423731885176740dd37fc
Austin Browne <aus.bro22@gmail.com>
2018-03-09 19:47:58 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,9 @@
+var playlist = {
+  Toto: 'Africa'
+}
+function updatePlaylist(playlist, artistName, songTitle){
+    paylist.artistName = songTitle;
+}
+function removeFromPlaylist(playlist, artistName){
+
+}
